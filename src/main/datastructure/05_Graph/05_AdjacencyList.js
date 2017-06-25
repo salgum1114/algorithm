7177e804f0e8d87340408e85a02bfa46e4c895d4
salgum1114 <salgum1112@gmail.com>
2017-06-25 23:33:02 +0900

    Graph adjacencylist added

--- src/main/datastructure/05_Graph/05_AdjacencyList.js
+++ src/main/datastructure/05_Graph/05_AdjacencyList.js
@@ -1,6 +1,90 @@
 class AdjacencyList {
     constructor(params) {
+        this.obj = {};
+        this.length = 0;
+    }
+
+    insertVertex(vertex) {
+        const obj = {
+            [vertex]: [],
+        }
+        this.obj = Object.assign(this.obj, obj);
+        return;
+    }
+
+    insertEdge(u, v, weight) {
+        if (weight === null || typeof weight === 'undefined') {
+            return ;
+        }
+
+        const uIndex = Object.keys(this.obj).indexOf(u);
+        const vIndex = Object.keys(this.obj).indexOf(v);
         
+        if (uIndex === -1) {
+            console.log(`Tail vertex does not exist in Graph. u: ${u}, v: ${v}, weight: ${weight}`);
+            return;
+        }
+
+        if (vIndex === -1) {
+            console.log(`Head vertex does not exist in Graph. u: ${u}, v: ${v}, weight: ${weight}`);
+            return;
+        }
+
+        const obj = {
+            [v]: weight,
+        }
+
+        this.obj[u].push(obj);
+        return;
+    }
+
+    deleteEdge(u, v) {
+        const uIndex = Object.keys(this.obj).indexOf(u);
+        const vIndex = Object.keys(this.obj).indexOf(v);
+        
+        if (uIndex === -1) {
+            console.log(`Tail vertex does not exist in Graph. u: ${u}, v: ${v}, weight: ${weight}`);
+            return;
+        }
+
+        if (vIndex === -1) {
+            console.log(`Head vertex does not exist in Graph. u: ${u}, v: ${v}, weight: ${weight}`);
+            return;
+        }
+        // console.log(this.obj[u]);
+        const newArray = this.obj[u].filter((obj) => {
+            if (obj[v]) {
+                return false;
+            }
+            return true;
+        })
+        this.obj = Object.assign(this.obj, { [u]: newArray });
+    }
+
+    deleteVertex(vertex) {
+        const index = Object.keys(this.obj).indexOf(vertex);
+        if (index === -1) {
+            console.log(`The vertex dose not exist in Graph. vertex: ${vertex}`);
+            return;
+        }
+
+        delete this.obj[vertex];
+
+        Object.keys(this.obj).map((key) => {
+            const tempArray = this.obj[key].filter((obj) => {
+                if (obj[vertex]) {
+                    return false;
+                }
+                return true;
+            });
+            this.obj = Object.assign(this.obj, { [key]: tempArray });
+        });
+
+        return;
+    }
+
+    printGraph() {
+        console.log(this.obj);
     }
 }
 
--- src/main/datastructure/05_Graph/test.js
+++ src/main/datastructure/05_Graph/test.js
@@ -1,17 +1,34 @@
-const AdjacencyMatrix = require('./05_AdjacencyMatrix.js');
+// const AdjacencyMatrix = require('./05_AdjacencyMatrix.js');
 
-const Graph = new AdjacencyMatrix(4);
-for (var i = 0; i < 4; i++) {
-    Graph.insertVertex(i);
-}
-Graph.insertEdge(0, 1);
-Graph.insertEdge(0, 3);
-Graph.insertEdge(1, 0);
-Graph.insertEdge(1, 2);
-Graph.insertEdge(1, 3);
-Graph.insertEdge(2, 1);
-Graph.insertEdge(2, 3);
-Graph.insertEdge(3, 0);
-Graph.insertEdge(3, 1);
-Graph.insertEdge(3, 2);
-Graph.printMatrix();
+// const Graph = new AdjacencyMatrix(4);
+// for (var i = 0; i < 4; i++) {
+//     Graph.insertVertex(i);
+// }
+// Graph.insertEdge(0, 1);
+// Graph.insertEdge(0, 3);
+// Graph.insertEdge(1, 0);
+// Graph.insertEdge(1, 2);
+// Graph.insertEdge(1, 3);
+// Graph.insertEdge(2, 1);
+// Graph.insertEdge(2, 3);
+// Graph.insertEdge(3, 0);
+// Graph.insertEdge(3, 1);
+// Graph.insertEdge(3, 2);
+// Graph.printMatrix();
+
+const AdjacencyList = require('./05_AdjacencyList.js');
+
+const Graph = new AdjacencyList();
+Graph.insertVertex('a');
+Graph.insertVertex('b');
+Graph.insertVertex('c');
+
+Graph.insertEdge('b', 'a', 1);
+Graph.insertEdge('b', 'a', 4);
+Graph.insertEdge('a', 'b', 2);
+Graph.insertEdge('b', 'c', 3);
+Graph.insertEdge('c', 'c', 5);
+Graph.insertEdge('c', 'b', 6);
+
+Graph.deleteEdge('b', 'a');
+Graph.printGraph();
